9ea6fedd9e1cc6ef9e3a81ae46542dd3f8111118
default <evan.maddicks@stu.ocsb.ca>
2025-11-21 14:18:08 +0000

    fixed code creating error

--- main.ts
+++ main.ts
@@ -7,25 +7,25 @@
 const number = 246
 const change = number
 console.log(makeChange(change));
-function makeChange(cents: number): String {
+function makeChange(cents: number): string {
   let left = cents;
 
-  let toonies = Math.floor(left / 200);
+  const toonies = Math.floor(left / 200);
   left = left % 200;
 
-  let loonies = Math.floor(left / 100);
+  const loonies = Math.floor(left / 100);
   left = left % 100;
 
-  let quarters = Math.floor(left / 25);
+  const quarters = Math.floor(left / 25);
   left = left % 25;
 
-  let dimes = Math.floor(left / 10);
+  const dimes = Math.floor(left / 10);
   left = left % 10;
 
-  let nickels = Math.floor(left / 5);
+  const nickels = Math.floor(left / 5);
   left = left % 5;
 
-  let pennies = left;
+  const pennies = left;
 
   console.log("Your change is: " + toonies + " toonies, " + loonies + " loonies, " + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels and " + pennies + " cents")
 }
